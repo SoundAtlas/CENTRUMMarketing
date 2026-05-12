cefd6963624ffce8674118cc0dc6d76664053158
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-05-12 16:30:03 +0200

    Added basic CustomerService class

--- CENTRUMMarketing.App/Program.cs
+++ CENTRUMMarketing.App/Program.cs
@@ -1,21 +1,21 @@
 ﻿using CENTRUMMarketing.Core.Enums;
-using CENTRUMMarketing.Core.Models;
+using CENTRUMMarketing.Core.Services;
 namespace CENTRUMMarketing.App
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer(
-                1, "Nordic Design", "Mikkel Sørensen", "123456", "BLABLA@GMAIL.COM", "88888888", CustomerStatus.Lead);
+            CustomerService customerService = new CustomerService();
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.CompanyName);
-            Console.WriteLine(customer.ContactPerson);
-            Console.WriteLine(customer.Cvr);
-            Console.WriteLine(customer.Phone);
-            Console.WriteLine(customer.Email);
-            Console.WriteLine(customer.Status);
+            customerService.AddCustomer("Nordic Design", "Mikkel Sørensen", "123456", "BLABLA@GMAIL.COM", "88888888", CustomerStatus.Lead);
+            customerService.AddCustomer("Nordic Design Studio", "Mikkel Hansen", "654321", "BLABLABLA@GMAIL.COM", "99999999", CustomerStatus.Active);
+
+            foreach (var customer in customerService.GetAllCustomers())
+            {
+                Console.WriteLine($"{customer.Id}, {customer.CompanyName}, {customer.ContactPerson}, {customer.Email}, {customer.Phone}, {customer.Status}");
+            }
+
         }
     }
 }
--- CENTRUMMarketing.Core/Services/CustomerService.cs
+++ CENTRUMMarketing.Core/Services/CustomerService.cs
@@ -1,6 +1,40 @@
-﻿namespace CENTRUMMarketing.Core.Services
+﻿using CENTRUMMarketing.Core.Enums;
+using CENTRUMMarketing.Core.Models;
+
+namespace CENTRUMMarketing.Core.Services
 {
     public class CustomerService
     {
+        private List<Customer> _customers = new List<Customer>();
+        private int _nextId = 1;
+
+        public Customer AddCustomer(string companyName, string contactPerson, string cvr, string email, string phone, CustomerStatus status)
+        {
+            Customer customer = new Customer(_nextId, companyName, contactPerson, cvr, email, phone, status);
+
+            _customers.Add(customer);
+            _nextId++;
+
+            return customer;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _customers;
+        }
+
+        public Customer? GetCustomerById(int id)
+        {
+            foreach (var customer in _customers)
+            {
+                if (customer.Id == id)
+                {
+                    return customer;
+                }
+            }
+
+            return null;
+        }
+
     }
 }
