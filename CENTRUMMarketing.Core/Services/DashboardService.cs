8cb9f34945a4d96a824aa978995b1fae212cb728
VehementWhyrm <emmerencevml@outlook.com>
2026-05-12 23:56:48 +0200

    Adds WIP DashboardService changes and updates branch name.

--- CENTRUMMarketing.Core/Services/DashboardService.cs
+++ CENTRUMMarketing.Core/Services/DashboardService.cs
@@ -1,10 +1,28 @@
 ﻿using CENTRUMMarketing.Core.Enums;
 using CENTRUMMarketing.Core.Models;
 using CENTRUMMarketing.Core.Interfaces;
+using CENTRUMMarketing.Core.Repositories;
 
 namespace CENTRUMMarketing.Core.Services
 {
     public class DashboardService
     {
+        private readonly CustomerRepository _customerRepository;
+        private readonly TaskRepository _taskRepository;
+
+        public DashboardService(
+            CustomerRepository customerRepository,
+            TaskRepository taskRepository)
+        {
+            _customerRepository = customerRepository;
+            _taskRepository = taskRepository;
+        }
+
+        public int GetActiveCustomersCount()
+        {
+            return _customerRepository
+                .GetAll()
+                .Count(c => c.Status == CustomerStatus.Active);
+        }
     }
 }
